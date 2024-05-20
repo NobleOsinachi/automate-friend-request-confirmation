3db08a5721c0f71d6cf9703be972d79be8e23b9e
Noble Osinachi <nobleosinachi98@gmail.com>
2024-05-20 03:51:35 +0100

    Update manual.js

--- manual.js
+++ manual.js
@@ -1,14 +1,32 @@
+
+
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-while (true) {
-  const element = document.querySelector('[aria-label="Confirm"]');
-  if (element) {
-    setTimeout(() => {
-      element.click();
-    }, getRandomInt(1000, 5000)); // Random delay between 1 to 5 seconds
+let counter = 0;
+const elements = document.querySelectorAll('div[aria-label="Confirm"]');
+elements.forEach(element => {
+  setTimeout(() => {
+    element.click();
+    counter++;
+    if (counter % 5 === 0) {
+    
+ const sidebar =   document.querySelector('div[aria-label="Friend requests"] > div > div:nth-child(2)');
+      
+      sidebar.scrollTo(0, 10000000); //  Scrolls the sidebar to the bottom
 }
-}
+  }, getRandomInt(1000, 5000)); // Random delay between 1 to 5 seconds
+});
+
+
+
+
+
+
+
+
+
